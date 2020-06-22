b9694da9c2610425bbf44780a9adc9443c11115a
David Roe <roed.math@gmail.com>
2020-06-22 06:42:06 -0400

    Add setup.py and config.ini

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="psycodict",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["psycopg2>=2.8.5"],
+
+    # metadata for PyPI. setuptools only support single authors
+    license="GPL v2+",
+    author="David Roe",
+    author_email="roed.math@gmail.com",
+    description="dictionary-based python interface to PostgreSQL databases",
+    keywords="postgres database interface",
+    url="https://github.com/roed314/psycodict"
+)
